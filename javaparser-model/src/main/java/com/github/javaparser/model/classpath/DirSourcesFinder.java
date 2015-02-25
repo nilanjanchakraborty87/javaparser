cb19f013d688297ef68a55e762e6e3a42450cee5
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-25 11:45:06 +0000

    Commenting DirSourceFinder

--- javaparser-model/src/main/java/com/github/javaparser/model/classpath/DirSourcesFinder.java
+++ javaparser-model/src/main/java/com/github/javaparser/model/classpath/DirSourcesFinder.java
@@ -5,57 +5,75 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * Retrieves ClasspathElement from a directory.
+ * Represent a classpath element which corresponds to a directory.
  *
  * @author Federico Tomassetti
  */
 public class DirSourcesFinder implements ClasspathSource {
-	private File directory;
-	private String basePath;
+    private File directory;
+    private String basePath;
 
-	public DirSourcesFinder(File directory) {
-		this(directory, "");
-	}
+    /**
+     * Directory considered of the classpath.
+     * <p/>
+     * All the elements contained will have a path relative to this directory.
+     */
+    public DirSourcesFinder(File directory) {
+        this(directory, "");
+        if (!directory.isDirectory() || !directory.exists()) {
+            throw new IllegalArgumentException("An existing directory is expected");
+        }
+    }
 
-	private DirSourcesFinder(File directory, String basePath) {
-		if (!directory.exists()) {
-			throw new IllegalArgumentException("No such directory: " + directory.getAbsolutePath());
-		}
-		this.directory = directory;
-		this.basePath = basePath;
-	}
+    /**
+     * We explore the tree recursively, creating an instance for each subdirectory.
+     * <p/>
+     * The basePath specify the path of the directory represented by this, w.r.t. to the root of this ClasspathSource.
+     */
+    private DirSourcesFinder(File directory, String basePath) {
+        if (!directory.exists()) {
+            throw new IllegalArgumentException("No such directory: " + directory.getAbsolutePath());
+        }
+        this.directory = directory;
+        this.basePath = basePath;
+    }
 
-	
-	private Set<ClasspathSource> getSubtreesInternal() {
-		Set<ClasspathSource> subtrees = new HashSet<ClasspathSource>();
-		for (File child : directory.listFiles()) {
-			String path = basePath.isEmpty() ? child.getName() : basePath + "/" + child.getName();
-			if (child.isDirectory()) {
-				subtrees.add(new DirSourcesFinder(child, path));
-			}
-		}
-		return subtrees;
-	}
+    /**
+     * The subdirectories.
+     */
+    private Set<ClasspathSource> getSubtreesInternal() {
+        Set<ClasspathSource> subtrees = new HashSet<ClasspathSource>();
+        for (File child : directory.listFiles()) {
+            String path = basePath.isEmpty() ? child.getName() : basePath + "/" + child.getName();
+            if (child.isDirectory()) {
+                subtrees.add(new DirSourcesFinder(child, path));
+            }
+        }
+        return subtrees;
+    }
 
-	@Override
-	public Set<ClasspathElement> getElements() throws IOException {
-		Set<ClasspathElement> sourceFiles = new HashSet<ClasspathElement>();
+    @Override
+    public Set<ClasspathElement> getElements() throws IOException {
+        Set<ClasspathElement> sourceFiles = new HashSet<ClasspathElement>();
 
-		sourceFiles.addAll(getElementsInternal());
-		for (ClasspathSource subtree : getSubtreesInternal()) {
-			sourceFiles.addAll(subtree.getElements());
-		}
-		return sourceFiles;
-	}
-	
-	private Set<ClasspathElement> getElementsInternal() {
-		Set<ClasspathElement> elements = new HashSet<ClasspathElement>();
-		for (File child : directory.listFiles()) {
-			String path = basePath.isEmpty() ? child.getName() : basePath + "/" + child.getName();
-			if (child.isFile()) {
-				elements.add(new FileClasspathElement(path, child));
-			}
-		}
-		return elements;
-	}
+        sourceFiles.addAll(getElementsInternal());
+        for (ClasspathSource subtree : getSubtreesInternal()) {
+            sourceFiles.addAll(subtree.getElements());
+        }
+        return sourceFiles;
+    }
+
+    /**
+     * The actual files contained in the current directory.
+     */
+    private Set<ClasspathElement> getElementsInternal() {
+        Set<ClasspathElement> elements = new HashSet<ClasspathElement>();
+        for (File child : directory.listFiles()) {
+            String path = basePath.isEmpty() ? child.getName() : basePath + "/" + child.getName();
+            if (child.isFile()) {
+                elements.add(new FileClasspathElement(path, child));
+            }
+        }
+        return elements;
+    }
 }
